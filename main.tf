7845821e7d5412876cea3bc281ddaf2774ce60ce
Sam Hamilton <samilton@gmail.com>
2023-09-06 02:18:54 +0000

    Initial check (#1)
    Initial setup of a terraform project with format check

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+
+}
+
+resource "aws_instance" "main_server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  network_interface {
+    network_interface_id = var.network_interface_id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+
+resource "ebs_volume" "main_volume" {
+
+}

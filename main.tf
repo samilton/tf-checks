3d054da96e62533a8d4d9c08ec2bf6b4ce1a11ce
Sam Hamilton <samilton@gmail.com>
2023-09-05 22:32:30 -0400

    fixed formatting

--- main.tf
+++ main.tf
@@ -37,12 +37,12 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "on_sns" {
-	  filename      = "lambda_function_payload.zip"
+  filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
 
-	  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs18.x"
 
